ddb5848ad034857c7ee3a22e642fa62962ec34da
rushikeshzope <144343549+rushikeshzope@users.noreply.github.com>
2023-11-02 17:01:40 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 // Iteration 2: Generate 2 random number and display it on the screen
 
+var number1 = Math.round(Math.random() * 100);
+const number1box = document.getElementById("number1")
+
+number.innerHTML
+
 // Iteration 3: Make the options button functional
 
 // Iteration 4: Build a timer for the game
